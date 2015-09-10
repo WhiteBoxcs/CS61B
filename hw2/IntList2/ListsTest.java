3bc5ee6ddbbcf2335198a2e9aced84df5a682e9e
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2015-09-10 00:51:01 -0700

    Adding branch hw2

---
+++ hw2/IntList2/ListsTest.java
@@ -0,0 +1,23 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** FIXME
+ *
+ *  @author FIXME
+ */
+
+public class ListsTest {
+    /** FIXME
+     */
+
+    // It might initially seem daunting to try to set up
+    // Intlist2 expected.
+   	//
+    // There is an easy way to get the IntList2 that you want in just
+    // few lines of code! Make note of the IntList2.list method that
+    // takes as input a 2D array.
+
+    public static void main(String[] args) {
+        System.exit(ucb.junit.textui.runClasses(ListsTest.class));
+    }
+}
