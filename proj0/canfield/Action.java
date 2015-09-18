d5b2aa2c1c3fcf64de1f60bbe02fd8191b094d2a
MadcowD <carlostnb@gmail.com>
2015-09-17 22:36:39 -0700

    Cleaning up the action interface.

--- proj0/canfield/Action.java
+++ proj0/canfield/Action.java
@@ -1,47 +1,22 @@
 /**
- * 
+ * The Action interface.
  */
 package canfield;
 
-import java.util.function.UnaryOperator;
-
 /**
+ * The action interface for bijective functions on gamestates.
  * @author MadcowD
  *
  */
-//class Action {
-//	UnaryOperator<Game> act;
-//	UnaryOperator<Game> undo;
-//	
-//	/**
-//	 * Initializes an action with respect to a game.
-//	 * @param game The game with which to initialize the action.
-//	 */
-//	public Action(UnaryOperator<Game> act, UnaryOperator<Game> undo){
-//		this.act = act;
-//		this.undo = undo;
-//	}
-//	
-//	/***
-//	 * Applies the action to the game.
-//	 * @param args The args required to apply the action.
-//	 */
-//	public void apply(Game game){
-//		act.apply(game);
-//	}
-//	
-//	/**
-//	 * Inverts a given action with respect to a game.
-//	 */
-//	public void inverseApply(Game game){
-//		undo.apply(game);
-//	}
-//}
-
 public interface Action{
+	/**
+	 * Acts in the forward direction (with respect to time)
+	 */
 	void act();
+	
+	/**
+	 * The direct inverse of Action.act (with respect to time)
+	 */
 	void undo();
 }
 
-//Due to the package protected paradigm that hilfinger sees fit,
-//I will store the following game actions in this file.
