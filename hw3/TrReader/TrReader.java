559560844cbd2998ac49595c9ace5c2c2930f077
MadcowD <carlostnb@gmail.com>
2015-09-21 22:17:35 -0700

    Finished homework 3

--- hw3/TrReader/TrReader.java
+++ hw3/TrReader/TrReader.java
@@ -1,69 +1,65 @@
+import java.io.IOException;
 import java.io.Reader;
 
 import com.sun.tools.javac.util.Assert;
 
-import java.io.IOException;
-
-/** Translating Reader: a stream that is a translation of an
- *  existing reader.
- *  @author
+/**
+ * Translating Reader: a stream that is a translation of an existing reader.
+ * 
+ * @author
  */
 public class TrReader extends Reader {
     private Reader source;
-<<<<<<< HEAD
-=======
-	private String from;
-	private String to;
->>>>>>> c6077f71cdda17424bc27078aa5f547b2c0b3730
+    private String from;
+    private String to;
 
-	/** A new TrReader that produces the stream of characters produced
-     *  by STR, converting all characters that occur in FROM to the
-     *  corresponding characters in TO.  That is, change occurrences of
-     *  FROM.charAt(0) to TO.charAt(0), etc., leaving other characters
-     *  unchanged.  FROM and TO must have the same length. */
+    /**
+     * A new TrReader that produces the stream of characters produced by STR, converting all
+     * characters that occur in FROM to the corresponding characters in TO. That is, change
+     * occurrences of FROM.charAt(0) to TO.charAt(0), etc., leaving other characters unchanged. FROM
+     * and TO must have the same length.
+     */
     public TrReader(Reader str, String from, String to) {
-    	Assert.check(from != null);
-    	Assert.check(to != null);
-    	Assert.check(from.length() == to.length());
-    	
+        Assert.check(from != null);
+        Assert.check(to != null);
+        Assert.check(from.length() == to.length());
+
         // FILL IN
-    	this.source = str;
-    	this.from = from;
-    	this.to = to;
+        this.source = str;
+        this.from = from;
+        this.to = to;
     }
 
-	@Override
-	public void close() throws IOException {
-		source.close();
-		
-	}
+    @Override
+    public void close() throws IOException {
+        this.source.close();
 
-	@Override
-	public int read(char[] cbuf, int off, int len) throws IOException {
-		int readStatus = source.read(cbuf, off, len);
+    }
 
-		for(int i = off; i < len; i++){
-		
-			cbuf[i-off] = map(cbuf[i-off]);
-		}
-		
-		return Math.min(len, readStatus);
-	}
+    @Override
+    public int read(char[] cbuf, int off, int len) throws IOException {
+        int readStatus = this.source.read(cbuf, off, len);
 
-	/**
-	 * Translates a given character.
-	 * @param origin
-	 * @return
-	 */
-	private char map(char origin){
-		int fromIndex = from.indexOf(origin);
-		if(fromIndex == -1)
-			return origin;
-		else
-			return to.charAt(fromIndex);
+        for (int i = off; i < len; i++) {
 
-		
-	}
+            cbuf[i] = this.map(cbuf[i]);
+        }
+
+        return Math.min(len, readStatus);
+    }
+
+    /**
+     * Translates a given character.
+     * 
+     * @param origin
+     * @return
+     */
+    private char map(char origin) {
+        int fromIndex = this.from.indexOf(origin);
+        if (fromIndex == -1)
+            return origin;
+        else
+            return this.to.charAt(fromIndex);
+
+    }
 }
-
-
--- hw3/WeirdList/WeirdListClient.java
+++ hw3/WeirdList/WeirdListClient.java
@@ -3,20 +3,24 @@ class WeirdListClient {
 
     /** Return the result of adding N to each element of L. */
     static WeirdList add(WeirdList L, int n) {
-        return null; // REPLACE THIS LINE WITH THE RIGHT ANSWER.
+        return L.map(x -> x + n);
     }
 
     /** Return the sum of the elements in L. */
     static int sum(WeirdList L) {
-        return 0; // REPLACE THIS LINE WITH THE RIGHT ANSWER.
+        Accumulator sum = new Accumulator(0);
+
+        L.map(sum);
+        return sum.result();
     }
 
-    /* As with WeirdList, you'll need to add an additional class or
-     * perhaps more for WeirdListClient to work. Again, you may put
-     * those classes either inside WeirdListClient as private static
+    /*
+     * As with WeirdList, you'll need to add an additional class or perhaps more for WeirdListClient
+     * to work. Again, you may put those classes either inside WeirdListClient as private static
      * classes, or in their own separate files.
-
-     * You are still forbidden to use any of the following:
-     *       if, switch, while, for, do, try, or the ?: operator.
+     * 
+     * You are still forbidden to use any of the following: if, switch, while, for, do, try, or the ?:
+     * operator.
      */
+
 }
