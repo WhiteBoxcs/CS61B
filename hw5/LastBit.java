ad9f0538c6ca329f5aa3955f70437fa889a8e58d
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2015-10-08 21:21:25 -0700

    Adding branch hw5

---
+++ hw5/LastBit.java
@@ -0,0 +1,19 @@
+/** Bit-twiddling class.
+ *  @author
+ */
+public class LastBit {
+
+    /** Returns the number N!=0 with all but its last (least significant)
+     *  1-bit set to 0.
+     *
+     *  For example
+     *      lastBit(3) == lastBit(7) == lastBit(1145) == 1,
+     *  and
+     *      lastBit(4) == lastBit(12) == lastBit(2052) == 4.
+     */
+    public static int lastBit(int n) {
+        return 0; // REPLACE *ONLY* 0 WITH SOLUTION.
+                  //DO NOT ADD EXTRA STATEMENTS.
+    }
+
+}
---
+++ hw5/Nybbles.java
@@ -0,0 +1,49 @@
+/** Represents an array of integers each in the range -8..7.
+ *  Such integers may be represented in 4 bits (called nybbles).
+ *  @author
+ */
+public class Nybbles {
+
+    /** Maximum positive value of a Nybble. */
+    public static final int MAX_VALUE = 7;
+
+    /** Return an array of size N. */
+    public Nybbles(int N) {
+        // DON'T CHANGE THIS.
+        _data = new int[(N + 7) / 8];
+        _n = N;
+    }
+
+    /** Return the size of THIS. */
+    public int size() {
+        return _n;
+    }
+
+    /** Return the Kth integer in THIS array, numbering from 0.
+     *  Assumes 0 <= K < N. */
+    public int get(int k) {
+        if (k < 0 || k >= _n) {
+            throw new IndexOutOfBoundsException();
+        } else {
+            return 0; // REPLACE WITH SOLUTION
+        }
+    }
+
+    /** Set the Kth integer in THIS array to VAL.  Assumes
+     *  0 <= K < N and -8 <= VAL < 8. */
+    public void set(int k, int val) {
+        if (k < 0 || k >= _n) {
+            throw new IndexOutOfBoundsException();
+        } else if (val < (-MAX_VALUE - 1) || val > MAX_VALUE) {
+            throw new IllegalArgumentException();
+        } else {
+            _data[0] = 0; // REPLACE WITH SOLUTION
+        }
+    }
+
+    // DON'T CHANGE OR ADD TO THESE.
+    /** Size of current array (in nybbles). */
+    private int _n;
+    /** The array data, packed 8 nybbles to an int. */
+    private int[] _data;
+}
