4381ad81de1f8975b89ab6ced92826f834f3a79e
William Guss <carlostnb@gmail.com>
2015-10-21 10:26:11 -0700

    Finished hw6

---
+++ hw6/BSTDictionary.java
@@ -0,0 +1,73 @@
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author william
+ *
+ */
+public class BSTDictionary implements SimpleDictionary {
+
+	/* (non-Javadoc)
+	 * @see SimpleDictionary#put(java.lang.String, java.lang.String)
+	 */
+	@Override
+	public void put(String word, String definition) {
+		KVP data = new KVP(word, definition);
+		
+		store.put(data);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see SimpleDictionary#contains(java.lang.String)
+	 */
+	@Override
+	public boolean contains(String word) {
+		return store.contains(new KVP(word,null));
+	}
+
+	/* (non-Javadoc)
+	 * @see SimpleDictionary#get(java.lang.String)
+	 */
+	@Override
+	public String get(String word) {
+		 KVP val = store.get(new KVP(word, null));
+		 if(val != null)
+			 return val.getValue();
+		 else
+			 return null;
+	}
+	
+	BSTGenericSet<KVP> store = new BSTGenericSet<KVP>();
+	
+	
+	private class KVP implements Comparable{
+		private String key;
+		private String value;
+
+		public KVP(String key, String value){
+			this.key = key;
+			this.value = value;
+		}
+
+		public String getKey() {
+			return key;
+		}
+
+		public String getValue() {
+			return value;
+		}
+
+		@Override
+		public int compareTo(Object o) {
+			KVP other = (KVP)o;
+			
+			return getKey().compareTo(other.getKey());
+		}
+
+	}
+
+}
---
+++ hw6/BSTGenericSet.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+
+/**
+ * @author william
+ *
+ */
+public class BSTGenericSet<Y extends Comparable> 
+	implements GenericSet<Y> {
+
+	public BSTGenericSet() {
+		this(null, null, null);
+	}
+	
+	public BSTGenericSet(Y value, 
+			BSTGenericSet<Y> left, BSTGenericSet<Y> right){
+		this._left = left;
+		this._right = right;
+		this._value = value;
+	}
+	
+	public BSTGenericSet(Y value){
+		this(value,null,null);
+	}
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see GenericSet#put(java.lang.Generic)
+	 */
+	@Override
+	public void put(Y s) {
+		if(_value == null)
+			this._value = s;
+		else
+		{
+			int compr = s.compareTo(_value);
+			if(compr > 0)
+			{
+				if(_right == null)
+					_right = new BSTGenericSet<Y>(s);
+				else
+					_right.put(s);
+			}
+			else if(compr < 0){
+				if(_left == null)
+					_left = new BSTGenericSet<Y>(s);
+				else
+					_left.put(s);
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see GenericSet#contains(java.lang.Generic)
+	 */
+	@Override
+	public boolean contains(Y s) {
+		if(_value != null)
+		{
+			int compr = s.compareTo(_value);
+			if(compr == 0)
+				return true;
+			else if(compr < 0 && _left != null)
+					return _left.contains(s);
+			else if(compr > 0 && _left != null)
+				return _right.contains(s);
+		}
+		
+		
+		return false;
+	}
+	
+	public Y get(Y s){
+		if(_value != null)
+		{
+			int compr = s.compareTo(_value);
+			if(compr == 0)
+				return _value;
+			else if(compr < 0 && _left != null)
+					return _left.get(s);
+			else if(compr > 0 && _left != null)
+				return _right.get(s);
+		}
+		
+		
+		return null;
+	}
+	
+	public boolean isEmpty() {
+		return _value == null;
+	}
+	
+	
+	private BSTGenericSet<Y> _left = null;
+	private BSTGenericSet<Y> _right = null;
+	private Y _value = null;
+}
