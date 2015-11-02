7ce2f517ae78647db4f578606388de31c437d37c
Paul Hilfinger <Hilfinger@cs.berkeley.edu>
2015-11-02 04:08:13 -0800

    Adding branch proj2

---
+++ proj2/loa/UnitTest.java
@@ -0,0 +1,27 @@
+// Remove all comments that begin with //, and replace appropriately.
+// Feel free to modify ANYTHING in this file.
+package loa;
+
+import ucb.junit.textui;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/** The suite of all JUnit tests for the loa package.
+ *  @author
+ */
+public class UnitTest {
+
+    /** Run the JUnit tests in the loa package. Add xxxTest.class entries to
+     *  the arguments of runClasses to run other JUnit tests. */
+    public static void main(String[] ignored) {
+        textui.runClasses(UnitTest.class);
+    }
+
+    /** A dummy test to avoid complaint. */
+    @Test
+    public void placeholderTest() {
+    }
+
+}
+
+
