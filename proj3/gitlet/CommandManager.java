b37be8b6dd0ae2ae5a6a1b91c03a061f5f7c9d45
William Guss <carlostnb@gmail.com>
2015-12-05 16:44:19 -0800

    Command manager.,

---
+++ proj3/gitlet/CommandManager.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package gitlet;
+
+/**
+ * @author william
+ *
+ */
+public class CommandManager {
+
+}
--- proj3/gitlet/Main.java
+++ proj3/gitlet/Main.java
@@ -8,6 +8,9 @@ public class Main {
     /** Usage: java gitlet.Main ARGS, where ARGS contains
      *  <COMMAND> <OPERAND> .... */
     public static void main(String... args) {
+    	CommandManager processor = new CommandManager();
+    	processor.add("init", new InitCommand());
+    	
         // FILL THIS IN
     }
 
