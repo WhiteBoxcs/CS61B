9b5d5bf5ee6d1b48482222bed2d3f11f62598ec9
William Guss <carlostnb@gmail.com>
2015-12-05 17:11:59 -0800

    Command code update.

--- proj3/gitlet/CommandManager.java
+++ proj3/gitlet/CommandManager.java
@@ -3,10 +3,29 @@
  */
 package gitlet;
 
+import java.util.HashMap;
+
 /**
  * @author william
  *
  */
 public class CommandManager {
+	private HashMap<String,Command> commandMap;
 
+
+	public void CommandManager(){
+		this.commandMap = new HashMap<>();
+	}
+	
+	
+	public void add(String trigger, Command newCommand){
+		this.commandMap.put(trigger, newCommand);
+	}
+	
+	public void process(String args[]) throws Exception{
+		String command = args[0];
+		if(!commandMap.containsKey(key)){
+			
+		}
+	}
 }
