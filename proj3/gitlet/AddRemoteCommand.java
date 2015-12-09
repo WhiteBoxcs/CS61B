a36fca404aaf0eb4de9b027eefeb1fb53d84199f
William Guss <carlostnb@gmail.com>
2015-12-08 23:52:09 -0800

    Adding the first remote command.

---
+++ proj3/gitlet/AddRemoteCommand.java
@@ -0,0 +1,38 @@
+/**
+ *
+ */
+package gitlet;
+
+/**
+ * @author william
+ */
+public class AddRemoteCommand implements Command {
+
+    /*
+     * (non-Javadoc)
+     * @see gitlet.Command#run(gitlet.Repository, java.lang.String[])
+     */
+    @Override
+    public void run(Repository repo, String[] args) {
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see gitlet.Command#requiresRepo()
+     */
+    @Override
+    public boolean requiresRepo() {
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see gitlet.Command#checkOperands(java.lang.String[])
+     */
+    @Override
+    public boolean checkOperands(String[] args) {
+        return args.length == 2;
+    }
+
+}
